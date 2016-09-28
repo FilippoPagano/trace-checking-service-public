de6f214ef33b6a60f99744473efa431469da017e
micheleguerriero <michele.guerriero2391@gmail.com>
2016-09-28 11:14:22 +0200

    Project initialization.

---
+++ initializer/src/main/java/it/polimi/cassandra/utils/initializer/CassandraInitializer.java
@@ -0,0 +1,13 @@
+package it.polimi.cassandra.utils.initializer;
+
+/**
+ * Hello world!
+ *
+ */
+public class CassandraInitializer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
