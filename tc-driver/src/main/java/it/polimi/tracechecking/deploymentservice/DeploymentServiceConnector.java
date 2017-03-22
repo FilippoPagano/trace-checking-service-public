87cb50beb042ed6676724108feb0cfcad7cd3591
FilippoPagano <filippo.pagano@mail.polimi.it>
2017-03-22 14:30:51 +0100

    Connector
    Filled some connect and getCluster methods

--- tc-driver/src/main/java/it/polimi/tracechecking/deploymentservice/DeploymentServiceConnector.java
+++ tc-driver/src/main/java/it/polimi/tracechecking/deploymentservice/DeploymentServiceConnector.java
@@ -1,8 +1,17 @@
 package it.polimi.tracechecking.deploymentservice;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 import it.polimi.tracechecking.common.model.ComputeNode;
 import it.polimi.tracechecking.common.model.DIA;
 import it.polimi.tracechecking.common.model.DIAElement;
@@ -11,13 +20,91 @@ import it.polimi.tracechecking.common.model.StorageSystem;
 public class DeploymentServiceConnector {
     
     private static List<VmsCluster> clusters = new ArrayList<VmsCluster>();
-
+	private static String address;
+	private static String token;
+    
     public void connect(String contactPoint, Integer port){
+        try {
+       	 	  address=contactPoint;
+			  URL obj = new URL(new URL(contactPoint), "auth/get-token/");  
+			  HttpURLConnection con = (HttpURLConnection) obj.openConnection();  
+			  
+			  // Setting basic post request  
+			  con.setRequestMethod("POST");     
+			  con.setRequestProperty("Content-Type","application/json");  
+			  String postJsonData = "{\"username\":\"dice\",\"password\":\"qqfTOxUbEnmX55jOBqGC\"}";  
+			    
+			  // Send post request  
+			  con.setDoOutput(true);  
+			  DataOutputStream wr = new DataOutputStream(con.getOutputStream());  
+			  wr.writeBytes(postJsonData);  
+			  wr.flush();  
+			  wr.close();  
+			  
+			  int responseCode = con.getResponseCode();  
+			  System.out.println("\nSending 'POST' request to URL : " + obj.toString());  
+			  System.out.println("Post Data : " + postJsonData);  
+			  System.out.println("Response Code : " + responseCode);  
+			  
+			  BufferedReader in = new BufferedReader(  
+			  new InputStreamReader(con.getInputStream()));  
+			  String output;  
+			  StringBuffer response = new StringBuffer();  
+			  
+			  while ((output = in.readLine()) != null) {  
+			   response.append(output);  
+			  }  
+			  in.close();  
+			    
+			  //printing result from response  
+			  JSONObject Jobj = new JSONObject(response.toString());
+			  System.out.println(Jobj.toString(1));  
+			  token = Jobj.getString("token");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
     }
     
     public VmsCluster getCluster(String clusterId){
-        return null;
+    	try {
+    	//TODO: waiting for APIs update...
+ 
+	  URL obj = new URL(new URL(address), "containers/"+ clusterId + "/nodes");
+	  HttpURLConnection con = (HttpURLConnection) obj.openConnection();  
+	        // Setting basic post request  
+	  con.setRequestMethod("GET");   
+	  con.setRequestProperty("Content-Type","application/json");  
+	  con.setRequestProperty("Authorization","Token " + token);  
+	  
+	  //Sending request
+	  int responseCode = con.getResponseCode();  
+	  
+	  //Reading response
+	  BufferedReader in = new BufferedReader(  
+	  new InputStreamReader(con.getInputStream()));  
+	  String output;  
+	  StringBuffer response = new StringBuffer();  
+	  while ((output = in.readLine()) != null) {  
+	   response.append(output);  
+	  }  
+	  in.close();  
+	    
+	  //printing result from response  
+	  System.out.println("\nSending 'POST' request to URL : " + obj.toString());   
+	  System.out.println("Response Code : " + responseCode);
+	  JSONArray Jobj = new JSONArray(response.toString());
+	  System.out.println(Jobj.toString(1));
+	  
+    	} catch (/*MalformedURLException | ProtocolException |*/ IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}  
+    		VmsCluster cluster = new VmsCluster(clusterId);
+    		//TODO: set addresses list
+    		//cluster.setAddresses(List<String> addresses);
+    	 return cluster;
     }
     
     public List<VmsCluster> getClusters(DIA dia){
@@ -35,4 +122,16 @@ public class DeploymentServiceConnector {
     public void close() {
         
     }
+    /*
+	public static void main(String args[]) {
+		try {
+		//	connect("",0);
+			connect("http://109.231.122.194/",0);
+			getCluster( "0b1f2a83-9ded-4984-bf46-8024e6a20cb5");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
 }
