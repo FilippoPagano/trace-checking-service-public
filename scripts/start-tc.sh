b7d93587f54dd1aa43967a752aac831e3022599c
FilippoPagano <filippo.pagano@mail.polimi.it>
2017-07-27 19:54:12 +0200

    Fix hostname in start script

--- scripts/start-tc.sh
+++ scripts/start-tc.sh
@@ -7,6 +7,7 @@
 
 export HADOOP_HOME=$HOME/hadoop-2.6.0
 export SPARK_HOME=$HOME/spark-1.4.1-bin-hadoop2.6
+export HOSTNAME='hostname'
 
 $HADOOP_HOME/bin/hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
@@ -16,7 +17,7 @@ $HADOOP_HOME/bin/hdfs dfs -mkdir /user/$USER
 
 $SPARK_HOME/sbin/start-master.sh
 
-$SPARK_HOME/sbin/start-slave.sh
+$SPARK_HOME/sbin/start-slave.sh $HOSTNAME:7077
 
 sudo service apache2 start
 
